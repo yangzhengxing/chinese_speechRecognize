31852f7208fc2c897b0b8fd02f3987c27f7b10f4
Yun Huang <huangyunict@gmail.com>
2016-04-19 22:18:49 -0500

    add clean

--- clean_auto_files.sh
+++ clean_auto_files.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-rm -rf Makefile.in aclocal.m4 autom4te.cache/ autoscan.log config.h.in configure missing
+rm -rf Makefile.in Makefile 
+rm -rf aclocal.m4 autom4te.cache/ 
+rm -rf autoscan.log 
+rm -rf config.h.in config.h config.log config.status
+rm -rf configure missing stamp-h1
 
-rm -rf install-sh confdefs.h conftest.dir/ depcomp install-sh
+rm -rf install-sh confdefs.h conftest.dir/ depcomp install-sh compile
 
 exit 0
 
