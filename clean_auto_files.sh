200c2549f61925cc4f08e87e396bc9fc2fc2a21a
Yun Huang <huangyunict@gmail.com>
2016-04-19 22:22:58 -0500

    add clean object files

--- clean_auto_files.sh
+++ clean_auto_files.sh
@@ -5,8 +5,9 @@ rm -rf aclocal.m4 autom4te.cache/
 rm -rf autoscan.log 
 rm -rf config.h.in config.h config.log config.status
 rm -rf configure missing stamp-h1
-
 rm -rf install-sh confdefs.h conftest.dir/ depcomp install-sh compile
+
+rm -rf src/*.o
 
 exit 0
 
